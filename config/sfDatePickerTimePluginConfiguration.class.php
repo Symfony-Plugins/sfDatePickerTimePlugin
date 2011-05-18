ac9574e30b1a2daae0fa0d15802087630f83c6b9
ldath <ldath@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2011-05-18 16:35:59 -0500

    creating version with bugfixes based on Pear package
    git-svn-id: http://svn.symfony-project.com/plugins/sfDatePickerTimePlugin/trunk@32540 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ config/sfDatePickerTimePluginConfiguration.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * sfDatePickerTimePlugin configuration.
+ *
+ * @package     sfDatePickerTimePlugin
+ * @subpackage  config
+ * @author      Arkadiusz Tułodziecki
+ * @version     SVN: $Id$
+ */
+class sfDatePickerTimePluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '1.0.0-DEV';
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
